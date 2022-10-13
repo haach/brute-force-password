c770e8c9ef9094b5aaf5dc35ac3c7b3e05bd05da
hanna 🍌 <hanna@iamhanna.de>
2022-10-13 21:21:10 +0200

    Improve App test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,21 @@
 import { describe, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from 'src/App';
 
 describe('App', () => {
   it('Renders the app', () => {
     render(<App />);
     expect(
-      screen.getByText('Brute force hack my password!')
+      screen.getByRole('heading', { name: 'Brute force hack my password' })
     ).toBeInTheDocument();
+  });
+  it('Allows for input', () => {
+    render(<App />);
+    const passInput = screen.getByRole('textbox', {
+      name: 'Your test password',
+    });
+    fireEvent.change(passInput, { target: { value: 'abcde' } });
+    const submitButton = screen.getByRole('button', { name: 'Hack it!' });
+    expect(submitButton).not.toBeDisabled();
   });
 });
